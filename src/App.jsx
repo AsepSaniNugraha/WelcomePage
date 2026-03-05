c62d3e597e4bdd18cfc00b949cfb693eb5a1657e
AsepSaniNugraha <asepsaninugraha@gmail.com>
2026-03-05 22:40:37 +0700

    Switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import ThemeButton from './components/ThemeButton'
 import WelcomePage from './components/WelcomePage'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ThemeButton />
       <Routes>
         <Route path='/' element={<WelcomePage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
